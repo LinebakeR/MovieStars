4941ce0f6fbe8c5ba4259f10558f7e418fde755a
alexg <alexandre.gracias@coding-academy.fr>
2019-04-08 19:18:11 +0200

    register

--- MovieStars/src/Controller/RegisterController.php
+++ MovieStars/src/Controller/RegisterController.php
@@ -2,12 +2,21 @@
 
 namespace App\Controller;
 
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environnement;
 
-class RegisterController{
+class RegisterController extends AbstractController{
 
+    /**
+    *@Route("/register", name="register.index");
+    *@return Response
+    */
+    public function index() : Response{
+
+    return new Response ('Page d\'enregistrement');
+}
 
     
 
