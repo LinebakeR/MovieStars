77385c84622c54a465cc72fbc96d544b50e5f9d1
alexg <alexandre.gracias@coding-academy.fr>
2019-04-09 17:13:05 +0200

    maj

--- MovieStars/src/Controller/HomeController.php
+++ MovieStars/src/Controller/HomeController.php
@@ -8,6 +8,7 @@ use Twig\Environment;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Tmdb\Api\Movies;
 
 
 
@@ -22,10 +23,12 @@ class HomeController extends AbstractController{
         $token  = new ApiToken("cea3e776cf0ad1a0e764b72aa0236425");
         $client = new Client($token);
         $movie = $client->getMoviesApi()->getMovie(550);
+        
  
         dump($movie);
         return $this->render('pages/home.html.twig', [
-            'movies' => $movie
+            'movies' => $movie,
+            
         ]);
     }
 
